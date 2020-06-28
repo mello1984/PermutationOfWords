0a4056aa69fd73b16e3b5c4750c515203eafabb2
Aleksandr Butakov <abutakov84@gmail.com>
2020-06-28 13:14:19 +0300

    Add simplify function

--- src/words/permutation/Analyzer.java
+++ src/words/permutation/Analyzer.java
@@ -4,19 +4,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Analyzer {
     private static final String testSequence1 = "ab aa ba ac ab ca cc Aa Ab ba bA aC CC bb Ca BB";
-    private static final String testSequence2 = "Hl Ui fz Ij jj Zq ys lH jj jI sr EX Cy zf ks lN mm XE yC Nl yc sy ul MM " +
-            "DE lu fD rs ED OO iU qZ sk cy Df ss";
-    private static final String testSequence3 = "WG vj NU ar GP DB hI ew YY Lf WW bm EE sc Qy ql lq LD ae pe mb mJ JQ Xe " +
-            "iH Qo wO jj Pq gT An gY KP zO UN PY sz kk zj Do xx Qx Tg JM ft Ow nA jh sb qP tf BD hj Vv zs Pq Cc iT xp gX " +
-            "Yg eX Hi rj oQ qI SX xZ oQ Ti ra OT Ih Zx OV ea zz aI Nc rL HH px oa qP XP ut yQ MM wF Zv FF ii OO xx cN MM " +
-            "Ae PG EE xQ AT YY tu XS DL TO cs Lr Xg jv jz PK kB Qo pp jj oQ eA cC Jm Ia Rh gx Wn fL QJ Mc NL TA OO tt Hg " +
-            "DI Bk Iq PX yy hR ut wS jr MJ cM YP oD gH nW rr tu dd tJ Fw Oz SX XS Jt we Sw vP GW bs xg VO ao Qo we kk Pv " +
-            "LN kk ew ep vZ ID xx vV";
+    private static final String testSequence2 = "rj KK YB Jr CC jr JJ lm JK DD dD rr dd MM BY Cr mm DU GG rC YY VG Mr KJ Dd GV rJ UD rM ml";
+    private static final String testSequence3 = "pE tZ pp ee ii dd OY mm BB Dx mm Do NN IX ps eE vS XE Ru Zt yU HH AA xD Ck " +
+            "Gn EE LL Nx CC Sv YY hh gw hh Xm aP Rt LA bh vv tt NR vS GG mP dA NN NN Ee eH LL kC PP En AX ZG BB wg hn Bz Ep " +
+            "hb OO uu xx GZ He Cw Ld KK Ad pE BB OO ku vh Sv Qo nh LT bb AA nG hM EE oQ CC sp Pg PP mX DD dL SS gP SS Py LL " +
+            "ji EX RA Pa AR ss AL ZZ Uy Et KH RN OO xN wC Pm XI YO ij hv gg Ep Mh yP Eb oo BB QQ TL II bE yy ww nE uu bb tE " +
+            "uR Ng zB PP pp gN tR NN HK XA GG tt uk oD";
     private static final String testSequence4 = "a b aa ba ab";
+    private static final String testSequence5 = "as b aa ba ab";
     private static final String fileNameForTesting = "d:/test/test3.txt";
     private static final String fileNameForGeneration = "d:/test/test5.txt";
 
@@ -24,19 +22,19 @@ public class Analyzer {
     public static void main(String[] args) throws IOException {
 //        String[] words = Files.lines(Paths.get(fileNameForTesting)).flatMap(s -> Arrays.stream(s.split("\\s+"))).toArray(String[]::new);
         String[] words = testSequence2.split("\\s");
-        check(words);
-        StringBuilder orderedWords = getLine(words);
         System.out.println("Original array - " + words.length + " words: " + Arrays.toString(words));
+        String[] simple = checkAndSimplify(words);
+        StringBuilder orderedWords = getLine(simple);
         if (orderedWords.length() > 0)
             System.out.println("Changed array - " + orderedWords.toString().split("\\s").length + ": " + orderedWords.toString());
-//        System.out.println(generateArray(10, 0.2));
+//        System.out.println(generateArray(50));
     }
 
     //Maybe I should make exceptions instead of messages
-    private static boolean check(String... words) {
+    private static String[] checkAndSimplify(String... words) {
         if (words == null || words.length == 0) {
             System.out.println("The array of words is invalid, " + words == null ? "it's null" : "it's length is 0");
-            return false;
+            return new String[0];
         }
 
         Map<Character, Integer> firstLetters = new HashMap<>();
@@ -49,16 +47,44 @@ public class Analyzer {
 
         if (firstLetters.size() != lastLetters.size()) {
             System.out.println("The array of words is invalid. The number of unique characters differs in the first and last character arrays");
-            return false;
+            return new String[0];
         }
 
         for (Map.Entry<Character, Integer> pair : firstLetters.entrySet())
             if (pair.getValue() != lastLetters.get(pair.getKey())) {
                 System.out.printf("The array of words is invalid. The number of '%s' character in first letters is %d, and in last letters is %d%n", pair.getKey(), pair.getValue(), lastLetters.get(pair.getKey()));
-                return false;
+                return new String[0];
             }
         System.out.println("Verification passed, letters: " + firstLetters);
-        return true;
+        return simplify(words);
+    }
+
+    private static String[] simplify(String... words) {
+        List<String> differing = new ArrayList<>(Arrays.asList(Arrays.stream(words).filter(s -> Character.toLowerCase(s.charAt(0)) != Character.toLowerCase(s.charAt(s.length() - 1))).toArray(String[]::new)));
+        List<String> same = new ArrayList<>(Arrays.asList(Arrays.stream(words).filter(s -> Character.toLowerCase(s.charAt(0)) == Character.toLowerCase(s.charAt(s.length() - 1))).toArray(String[]::new)));
+
+        System.out.println("differing: " + differing);
+        System.out.println("same: " + same);
+        System.out.println();
+        for (int i = same.size() - 1; i >= 0; i--) {
+            char ch = Character.toLowerCase(same.get(i).charAt(0));
+            int j = 0;
+            boolean next = false;
+            while (j < differing.size() && !next) {
+                char chD = Character.toLowerCase(differing.get(j).charAt(differing.get(j).length() - 1));
+                if (ch == chD) {
+                    differing.set(j, differing.get(j) + " " + same.remove(i));
+                    next = true;
+                }
+                j++;
+            }
+
+        }
+        System.out.println("differing: " + differing);
+        System.out.println();
+
+        return differing.toArray(new String[differing.size()]);
+
     }
 
     private static StringBuilder getLine(String... words) {
@@ -67,30 +93,24 @@ public class Analyzer {
         int initialCount = 0;
 
         while (!resolve) {
-            List<String> differing = new LinkedList(Arrays.asList(Arrays.stream(words).filter(s -> Character.toLowerCase(s.charAt(0)) != Character.toLowerCase(s.charAt(s.length() - 1))).toArray(String[]::new)));
-            List<String> same = new LinkedList(Arrays.asList(Arrays.stream(words).filter(s -> Character.toLowerCase(s.charAt(0)) == Character.toLowerCase(s.charAt(s.length() - 1))).toArray(String[]::new)));
+            List<String> list = new LinkedList(Arrays.asList(words));
 
-            int initialSame = same.size();
-            int initialDiffering = differing.size();
-            while (initialCount < differing.size() && !resolve) {
-
-                sb = new StringBuilder().append(differing.remove(initialCount));
-                while (same.size() > 0 || differing.size() > 0) {
-                    boolean added = checkListAndAddWord(sb, same);
-                    if (!added) added = checkListAndAddWord(sb, differing);
+            int initialDiffering = list.size();
+            while (initialCount < list.size() && !resolve) {
+                sb = new StringBuilder().append(list.remove(initialCount));
+                while (list.size() > 0) {
+                    boolean added = checkListAndAddWord(sb, list);
                     if (!added) break;
                 }
-
-                if (same.size() == 0 && differing.size() == 0) resolve = true;
+                if (list.size() == 0) resolve = true;
                 else {
                     initialCount++;
-                    System.out.printf("try: %3d, length: %3d, differing: %3d, same: %3d, all worlds: %3d, initial size initialDiffering: %3d, initialSame: %3d%n",
-                            initialCount, sb.toString().split("\\s").length, differing.size(), same.size(),
-                            (sb.toString().split(" ").length + differing.size() + same.size()), initialDiffering, initialSame);
+                    System.out.printf("try: %3d, length: %3d, initial size: %3d%n",
+                            initialCount, sb.toString().split("\\s").length, initialDiffering);
                     break;
                 }
             }
-            if (initialCount >= words.length - initialSame) {
+            if (initialCount >= words.length) {
                 System.out.println("The solution is not found");
                 return new StringBuilder();
             }
@@ -109,7 +129,7 @@ public class Analyzer {
 
             if (listFirst == sbLast || listLast == sbFirst) {
                 if (listFirst == sbLast) sb.append(" ").append(list.remove(i));
-                else sb.insert(0," ").insert(0, list.remove(i));
+                else sb.insert(0, " ").insert(0, list.remove(i));
                 next = true;
             }
             i++;
@@ -117,25 +137,21 @@ public class Analyzer {
         return next;
     }
 
-    private static String generateArray(int countHalf, double sameProbability) {
+    private static String generateArray(int countThird) {
         Random random = new Random();
         List<String> list = new LinkedList<>();
-        for (int i = 0; i < countHalf; i++) {
+        for (int i = 0; i < countThird; i++) {
             int x = (random.nextInt(26) + 65) + (Math.random() > 0.5 ? 32 : 0);
             int y = (random.nextInt(26) + 65) + (Math.random() > 0.5 ? 32 : 0);
-            if (Math.random() > sameProbability) {
-                list.add(String.valueOf((char) x) + (char) y);
-                list.add(String.valueOf((char) y) + (char) x);
-            } else {
-                list.add(String.valueOf((char) x) + (char) x);
-                list.add(String.valueOf((char) y) + (char) y);
-            }
+            list.add(String.valueOf((char) x) + (char) y);
+            list.add(String.valueOf((char) y) + (char) x);
+            list.add(String.valueOf((char) x) + (char) x);
         }
         Collections.shuffle(list);
         return String.join(" ", list);
     }
 
-    private static void generateArray(int countHalf, double sameProbability, String fileName) throws IOException {
-        Files.writeString(Paths.get(fileName), generateArray(countHalf, sameProbability));
+    private static void generateArray(int countThird, String fileName) throws IOException {
+        Files.writeString(Paths.get(fileName), generateArray(countThird));
     }
 }
